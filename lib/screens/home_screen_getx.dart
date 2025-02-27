4d922981488713bea730c4c9c6c4685bbbc52595
alam-2002 <140315417+alam-2002@users.noreply.github.com>
2025-02-27 10:48:04 +0530

    controller separated

--- lib/screens/home_screen_getx.dart
+++ lib/screens/home_screen_getx.dart
@@ -1,54 +1,7 @@
-import 'dart:async';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:speech_to_text/speech_recognition_result.dart';
-import 'package:speech_to_text/speech_to_text.dart';
 import 'package:avatar_glow/avatar_glow.dart';
-
-class SpeechController extends GetxController {
-  final SpeechToText speechToText = SpeechToText();
-  var speechEnabled = false.obs;
-  var spokenWords = "".obs;
-  var confidenceLevel = 0.0.obs;
-  var isListening = false.obs;
-
-  @override
-  void onInit() {
-    super.onInit();
-    initSpeech();
-  }
-
-  void initSpeech() async {
-    speechEnabled.value = await speechToText.initialize();
-  }
-
-  void startListening() async {
-    isListening.value = true;
-    confidenceLevel.value = 0;
-
-    await speechToText.listen(
-      onResult: onSpeechResult,
-      pauseFor: const Duration(seconds: 3),
-      onSoundLevelChange: (level) {
-        Timer(Duration(seconds: 3), () {
-          if (level < -1.5) {
-            stopListening();
-          }
-        });
-      },
-    );
-  }
-
-  void stopListening() async {
-    isListening.value = false;
-    await speechToText.stop();
-  }
-
-  void onSpeechResult(SpeechRecognitionResult result) {
-    spokenWords.value = result.recognizedWords;
-    confidenceLevel.value = result.confidence;
-  }
-}
+import 'package:my_speech_app/controller/speech_controller.dart';
 
 class HomeScreenGetx extends StatelessWidget {
   HomeScreenGetx({super.key});
